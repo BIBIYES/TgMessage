071cd1de3cefa01a4ec74d9ef116aae4e4d9d4c8
Mouse <mousehaozi@outlook.com>
2024-04-30 22:20:33 +0800

    1.6

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ async def main():
     main_message_link = 'https://t.me/shishixiaoxi'
     video_and_photo_group_link = 'https://t.me/shipinghetupian'
     decrypt_bot_link = "https://t.me/filespanbot"
-    db_name = "zihao"
+    db_name = "zihao.db"
     oneself_id = 6731268134  # 替换成你的 Telegram ID
 
     tg_module = TgModule(api_id, api_hash, main_message_link, video_and_photo_group_link, decrypt_bot_link, db_name,
@@ -28,7 +28,7 @@ async def main():
     main_message_link_2 = 'https://t.me/jiantingXP001'
     video_and_photo_group_link_2 = 'https://t.me/tuheshiXP001'
     decrypt_bot_link_2 = "https://t.me/filespanbot"
-    db_name_2 = "muzi"
+    db_name_2 = "muzi.db"
     oneself_id_2 = 6524572433  # 替换成你的 Telegram ID
 
     tg_module_2 = TgModule(api_id_2, api_hash_2, main_message_link_2, video_and_photo_group_link_2, decrypt_bot_link_2,
